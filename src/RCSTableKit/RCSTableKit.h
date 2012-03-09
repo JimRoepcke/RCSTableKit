e57dc8961e926df4e56de7f370cc61473d3dfcbc
Jim Roepcke <jim@roepcke.com>
2012-03-08 19:45:46 -0800

    updated copyright year

--- src/RCSTableKit/RCSTableKit.h
+++ src/RCSTableKit/RCSTableKit.h
@@ -17,7 +17,7 @@
 #import "RCSEditStringCell.h"
 
 /*
- * Copyright 2009-2011 Jim Roepcke <jim@roepcke.com>. All rights reserved.
+ * Copyright 2009-2012 Jim Roepcke <jim@roepcke.com>. All rights reserved.
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
